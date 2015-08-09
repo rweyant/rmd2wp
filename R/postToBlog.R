4f2608e94274ca503ca6f6c9f0ea9584700c9be0
rweyant <robert.weyant@gmail.com>
2015-08-09 17:27:56 -0400

    added CreateOrReplace functionality

--- R/postToBlog.R
+++ R/postToBlog.R
@@ -103,17 +103,43 @@ postToBlog <- function(file,
   }
   setwd(current.director)
 }
+# Determine title from .Rmd file
+determine_title <- function(file){
+  lines <- readLines(file)
+  title.lines <- lines[grepl('title:',lines)]
+  if(length(title.lines) == 1){
+    title <- str_split(title.lines,'title:')[[1]][2]
+    title <- str_replace_all(title,'\"','')
+    title <- str_trim(title,side = 'left')
+  } else if(length(title.lins) > 1){
+    stop('Title Ambiguous.')
+  }
+  return(title)
+}
 
+# Check if post with same title exists.  Returns that post's ID.
+checkPostExistence <- function(title){
+  posts <- RWordPress::getPosts()
+  # Find Post ID
+  postID <- as.numeric(as.character(posts[posts$title==title,'postid']))
+  if(length(postID)>1) stop('Ambiguous Post Title. Multiple posts exist with same title.')
+  return(ifelse(length(postID)==1,postID,NA))
+}
+
+# in .Rprofile
+# options(WordpressLogin=('username'='password'),
+#         WordpressURL='http://www.yourblog.com/xmlrpc.php')
 postToBlog.dev <- function(file,
                        title='',
                        categories=c(),
                        tags=c(),
-                       postID=NULL,
+                       postID=NA,
                        toc=TRUE,
-                       type=c('new','edit','createOrReplace'),
+                       type=c('new','edit','CreateOrReplace'),
                        syntax.highlighter='Syntax Highlighter Evolved',
                        publish=FALSE){
 
+  # Load dependencies
   library(RWordPress)
   library(knitr)
   library(markdown)
@@ -123,21 +149,21 @@ postToBlog.dev <- function(file,
   # Determine what kind of action to take
   type <- match.arg(type)
 
-  ## Determine file name of .html output file
+  # Determine file name of .html output file
   file.name <- basename(file)
   dir.name <- dirname(file)
-
   file.extension <- file_ext(file.name)
+  current.directory <- getwd()
+  setwd(dir.name)
 
-  # Should this be a stop()?
+  # Check that file passed was .Rmd
   if(file.extension!='Rmd'){
-    warning("File extension is not .Rmd.  Check input file to make sure it is correct format.")
+    stop("File extension is not .Rmd.  Check input file to make sure it is correct format.")
   }
 
-  file.root <- str_split(basename(file),pattern = paste('.',file.extension,sep=''))[[1]][1]
+  # Determine what the HTML file will be called
+  file.root <- str_split(file.name,pattern = paste('.',file.extension,sep=''))[[1]][1]
   html.file <- paste(file.root,'.html',sep='')
-  current.director <- getwd()
-  setwd(dir.name)
 
   cat('Directory:',dir.name,'\n')
   cat('RMarkdown file:', file.name,'\n')
@@ -146,79 +172,41 @@ postToBlog.dev <- function(file,
   cat('Action Type:', type,'\n')
   cat('Syntax Highlighter:',syntax.highlighter,'\n')
 
-  # in .Rprofile
-  # options(WordpressLogin=('username'='password'),
-  #         WordpressURL='http://www.yourblog.com/xmlrpc.php')
-
-  # Table of Contents
+  # Set some markdown options
   options(markdown.HTML.options =  c('use_xhtml','base64_images','mathjax','highlight_code',"toc","hard_wrap"))
-  # options(markdown.HTML.options =  c(markdownHTMLOptions(default = T),"number_sections"))
 
-  # Knit an HTML file
-  knit2html(file.name)
+  knit2html(file.name) # Knit an HTML file
 
   # Get Title From .Rmd
   if(is.null(title)){
-    lines <- readLines(file)
-    title.lines <- lines[grepl('title:',lines)]
-    if(length(title.lines) == 1){
-      title <- str_split(title.lines,'title:')[[1]][2]
-      title <- str_replace_all(title,'\"','')
-      title <- str_trim(title,side = 'left')
-    } else if(length(title.lins) > 1){
-      stop('Title Ambiguous.')
-    }
+    determine_title(file.name)
   }
-
 
   # Get Post ID
-  posts <- RWordPress::getPosts()
-  # Find Post ID
-  postID <- posts[posts$title==title,'postid']
+  if(is.na(postID)) postID <- checkPostExistence(title)
 
-
-  # New/Edit -- need title, post-id
-
-  ## NEW POST
-  if(type=='new'){
-    newPost(
-      list(
-        description=knit2wordpress(html.file,syntax.highlighter=syntax.highlighter),
-        title=title,
-        categories=categories,
-        mt_keywords=tags
-      ),
-      publish=publish)
-    ## EDIT POST
-  } else if(type=='edit') {
-    posts <- RWordPress::getPosts()
-    if(is.null(postID)){
-
-      # Find Post ID
-      postID <- posts[posts$title==title,'postid']
-      # Check that there is a post to edit
-      if(length(postID)==1 && !is.null(postID) && !is.na(postID)){
-        print(paste('Editing post [',postID,']: ',title,'.',sep=''))
-        editPost(postid = postID,
-                 content=list(description=knit2wordpress(html.file,syntax.highlighter=syntax.highlighter),
-                              title=title,
-                              categories=categories,
-                              mt_keyworkds=tags),
-                 publish = publish
-        )
-      }
-      ## Iff there is exactly 1 post that matches ID given
-    } else if(sum(posts$postid==postID) == 1){
-      print(paste('Editing post [',postID,']: ',title,'.',sep=''))
-      editPost(postid = postID,
-               content=list(description=knit2wordpress(html.file,syntax.highlighter=syntax.highlighter),
-                            title=title,
-                            categories=categories,
-                            mt_keyworkds=tags),
-               publish = publish
-      )
-    }
+  # Create New Post
+  if(is.na(postID) || type=='new') {
+    print('Creating New Post')
+    newPost(content=list(description=knit2wordpress(html.file,syntax.highlighter=syntax.highlighter),
+                         title=title,
+                         categories=categories,
+                         mt_keywords=tags),
+            publish=publish)
+  }  else if (!is.na(postID)) {
+    # Edit Existing Post
+    print('Editing Old Post')
+    editPost(postid = postID,
+             content=list(description=knit2wordpress(html.file,syntax.highlighter=syntax.highlighter),
+                          title=title,
+                          categories=categories,
+                          mt_keyworkds=tags),
+             publish = publish)
+  } else{
+    warning('Not Sure what to do.  No posts created or updated.')
   }
-  setwd(current.director)
+
+  # Go back to directory where function was called
+  setwd(current.directory)
 }
 
